4e0caf82fe8e68c1cb6f58029e53bec71ce10058
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-04-11 20:36:00 +0900

    commit

--- src/components/atom/Item.tsx
+++ src/components/atom/Item.tsx
@@ -8,9 +8,9 @@ type Props = {
 const Item: React.FC<Props> = ({ title, Pic }) => {
   // console.log(process.env.PUBLIC_URL);
   return (
-    <div className="flex flex-col w-full  md:w-auto md:max-w-md items-center cursor-pointer transform duration-150 hover:scale-105 mx-3 my-2">
+    <div className="flex flex-col  md:w-auto md:max-w-md items-center cursor-pointer transform duration-150 hover:scale-105 mx-3 my-2">
       <div className="border border-gray-800 rounded-lg items-center  justify-center grid place-items-center h-30 w-30 ">
-        <img src={`images/pic/${Pic}`} className=" w-full h-full md:w-32 md:h-32" />
+        <img src={`images/pic/${Pic}`} className=" md:w-32 md:h-32" />
       </div>
       <p className="text-xs font-mono items-center m-4">{title}</p>
     </div>
