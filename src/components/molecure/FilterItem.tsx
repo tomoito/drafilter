59481e341a708e653fb0592cd6528193977ce618
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-04-25 16:04:30 +0900

    commit

--- src/components/molecure/FilterItem.tsx
+++ src/components/molecure/FilterItem.tsx
@@ -18,6 +18,7 @@ const FilterItem: React.FC<Props> = ({ title }) => {
     <div className="overflow-x-scroll flex w-screen space-x-3 whitespace-nowrap items-center">
       {title.map(i => (
         <div
+          key={i}
           onClick={() => handleClick(i)}
           className=" bg-yellow-100 text-xs flex rounded-lg w-30 h-auto p-1 text-center items-center transform duration-150 hover:scale-110 hover:bg-gray-500 cursor-pointer mb-2">
           <p className="text-center">{i}</p>
